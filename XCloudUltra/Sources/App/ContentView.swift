34049e9160a0a82e1e4b70eead17d3833ff65596
marcusmack144 <marcusmack144@gmail.com>
2026-09-11 00:10:20 -0400

    Simplify unused SwiftUI content view

--- XCloudUltra/Sources/App/ContentView.swift
+++ XCloudUltra/Sources/App/ContentView.swift
@@ -1,39 +1,7 @@
 import SwiftUI
-import WebKit
 
-struct ContentView: UIViewRepresentable {
-    func makeCoordinator() -> Coordinator {
-        Coordinator()
-    }
-
-    func makeUIView(context: Context) -> WKWebView {
-        let contentController = WKUserContentController()
-        if let scriptURL = Bundle.main.url(forResource: "better-xcloud.user", withExtension: "js"),
-           let source = try? String(contentsOf: scriptURL, encoding: .utf8) {
-            let userScript = WKUserScript(
-                source: source,
-                injectionTime: .atDocumentEnd,
-                forMainFrameOnly: false
-            )
-            contentController.addUserScript(userScript)
-        }
-
-        let configuration = WKWebViewConfiguration()
-        configuration.userContentController = contentController
-        configuration.allowsInlineMediaPlayback = true
-        configuration.mediaTypesRequiringUserActionForPlayback = []
-
-        let webView = WKWebView(frame: .zero, configuration: configuration)
-        webView.navigationDelegate = context.coordinator.navigationHandler
-        webView.allowsBackForwardNavigationGestures = true
-        webView.scrollView.contentInsetAdjustmentBehavior = .never
-        webView.load(URLRequest(url: URL(string: "https://www.xbox.com/play")!))
-        return webView
-    }
-
-    func updateUIView(_ webView: WKWebView, context: Context) {}
-
-    final class Coordinator {
-        let navigationHandler = NavigationHandler()
+struct ContentView: View {
+    var body: some View {
+        Color.black.ignoresSafeArea()
     }
 }
