6d8557a7ed73f3d04a5eddbd948de74a37a5125d
marcusmack144 <marcusmack144@gmail.com>
2026-09-11 00:13:45 -0400

    Simplify WebKit scene for reliable compilation

--- XCloudUltra/Sources/App/SceneDelegate.swift
+++ XCloudUltra/Sources/App/SceneDelegate.swift
@@ -1,9 +1,8 @@
 import UIKit
 import WebKit
 
-final class SceneDelegate: UIResponder, UIWindowSceneDelegate, WKNavigationDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    private var webView: WKWebView?
 
     func scene(
         _ scene: UIScene,
@@ -12,59 +11,15 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate, WKNavigationDeleg
     ) {
         guard let windowScene = scene as? UIWindowScene else { return }
 
-        let contentController = WKUserContentController()
-        if let scriptURL = Bundle.main.url(forResource: "better-xcloud.user", withExtension: "js"),
-           let source = try? String(contentsOf: scriptURL, encoding: .utf8) {
-            contentController.addUserScript(
-                WKUserScript(
-                    source: source,
-                    injectionTime: .atDocumentEnd,
-                    forMainFrameOnly: false
-                )
-            )
-        }
-
-        let configuration = WKWebViewConfiguration()
-        configuration.userContentController = contentController
-        configuration.allowsInlineMediaPlayback = true
-        configuration.mediaTypesRequiringUserActionForPlayback = []
-
-        let webView = WKWebView(frame: .zero, configuration: configuration)
-        webView.navigationDelegate = self
-        webView.allowsBackForwardNavigationGestures = true
-        webView.scrollView.contentInsetAdjustmentBehavior = .never
+        let webView = WKWebView(frame: .zero)
         webView.load(URLRequest(url: URL(string: "https://www.xbox.com/play")!))
-        self.webView = webView
+
+        let rootViewController = UIViewController()
+        rootViewController.view = webView
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = WebViewController(webView: webView)
+        window.rootViewController = rootViewController
         self.window = window
         window.makeKeyAndVisible()
-    }
-
-    func webView(
-        _ webView: WKWebView,
-        decidePolicyFor navigationAction: WKNavigationAction,
-        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-    ) {
-        decisionHandler(.allow)
-    }
-}
-
-private final class WebViewController: UIViewController {
-    private let webView: WKWebView
-
-    init(webView: WKWebView) {
-        self.webView = webView
-        super.init(nibName: nil, bundle: nil)
-    }
-
-    @available(*, unavailable)
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func loadView() {
-        view = webView
     }
 }
